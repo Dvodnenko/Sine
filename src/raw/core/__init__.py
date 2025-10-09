ad97605bf5f37a6144d7ecdd2a74597244d863b4
Dvodnenko <thekovalskii@gmail.com>
2025-10-09 16:22:40 +0300

    feat: add engine & session

--- src/raw/core/__init__.py
+++ src/raw/core/__init__.py
@@ -1,4 +1,5 @@
 from .database.orm_registry import mapping_registry
+from .database.session import engine, Session
 
 
-__all__ = ("mapping_registry", )
+__all__ = ("mapping_registry", "engine", "Session",)
---
+++ src/raw/core/database/session.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from ...config import load_config
+
+
+conf = load_config()
+engine = create_engine(
+    conf.get("data_file"), 
+    echo=conf.get("echo")
+)
+Session = sessionmaker(bind=engine)
