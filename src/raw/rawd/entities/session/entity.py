c0863e82402cefdfb8baca27e98a28223be13521
Dvodnenko <thekovalskii@gmail.com>
2025-10-18 16:11:52 +0300

    feat: add method for auto generating session title

--- src/raw/rawd/entities/session/entity.py
+++ src/raw/rawd/entities/session/entity.py
@@ -1,13 +1,19 @@
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
+from hashlib import blake2b
 
 from ..base.entity import Entity
 from ..session.exceptions import SessionIsActiveError
 
 
+def generate_title():
+    return blake2b(digest_size=10).hexdigest()
+
+
 @dataclass(kw_only=True, eq=False)
 class Session(Entity):
     start: datetime
+    title: str = field(default_factory=generate_title)
     summary: str | None = field(default=None, kw_only=True)
     end: datetime | None = field(default=None, kw_only=True)
     _breaks: list[timedelta] = field(
