740a801dd1e9e69baac1f75d68753dc7a377fb5b
Dvodnenko <thekovalskii@gmail.com>
2025-10-29 17:09:57 +0200

    refactor: turn session service methods to generators

--- src/raw/rawd/services/sessions.py
+++ src/raw/rawd/services/sessions.py
@@ -14,66 +14,58 @@ class SessionService:
         self.active: Session | None = None
 
     @cast_kwargs(Session)
-    def begin(self, args: list, flags: list, **kwargs) -> tuple[str, int]:
-        print(f"kwargs: {kwargs}")
-        active = self.get_active()
+    def begin(self, args: list, flags: list, **kwargs):
+        active = next(self.repository.get_active())
         if active:
-            return f"Session is already started: '{active.title}'", 1
+            yield f"Session is already started: '{active.title}'", 1
+            return
         kwargs["start"] = kwargs.get("start") or \
             datetime.now().replace(microsecond=0)
         session = Session(**kwargs)
-        print(f"Session: {session}")
-        self.repository.create(session)
-        return f"Session started", 0
+        next(self.repository.create(session))
+        yield f"Session started", 0
     
     @cast_kwargs(Session)
     def stop(self, args: list, flags: list, **kwargs):
-        session = self.get_active()
+        session = next(self.repository.get_active())
         if not session:
-            return "Active Session not found", 1
+            yield "Active Session not found", 1
+            return
         kwargs["end"] = kwargs.get("end") or \
             datetime.now().replace(microsecond=0)
-        self.repository.update(session.title, **kwargs)
-        return "Session stoped", 0
-    
-    def get_active(self):
-        if self.active:
-            return self.active
-        return self.repository.get_active()
+        next(self.repository.update(session.title, **kwargs))
+        yield "Session stoped", 0
     
     @provide_conf
     def all(self, args: list, flags: list, **kwargs):
-        sortby = kwargs.get("sortby", "start")
-        sessions = self.repository.get_all()
-        sessions = sorted(
-            sessions,
-            key=lambda f: getattr(f, sortby),
-            reverse="r" in flags
-        )
+        sortby = kwargs.get("sortby", "title")
         if "t" in flags:
-            return "".join(f"{s.title}\n" for s in sessions)[:-1], 0
-        pattern: str = kwargs["__cnf"]["formats"]["session"]
-        return "".join([f"{pattern.format(
-            **s.to_dict()).rstrip()}\n" for s in sessions]).rstrip(), 0
+            for session in self.repository.get_all(sortby):
+                yield session.title, 0
+        else:
+            pattern: str = kwargs["__cnf"]["formats"]["session"]
+            for session in self.repository.get_all(sortby):
+                yield pattern.format(**session.to_dict()), 0
     
     @provide_conf
     def print(self, args: list, flags: list, **kwargs):
-        sessions = get_all_by_titles(self.repository.session, Session, args)
         pattern: str = kwargs["__cnf"]["formats"]["session"]
-        return "".join([f"{pattern.format(
-            **s.to_dict()).rstrip()}\n" for s in sessions]).rstrip(), 0
+        for session in get_all_by_titles(self.repository.session, Session, args):
+            yield pattern.format(**session.to_dict()), 0
     
     @cast_kwargs(Session)
     def update(self, args: list, flags: list, **kwargs):
-        current = self.repository.get(args[0])
+        current = next(self.repository.get(args[0]))
         if not current:
-            return f"Session not found: {args[0]}", 1
-        self.repository.update(args[0], **kwargs)
-        return f"Session updated: {args[0]}", 0
+            yield f"Session not found: {args[0]}", 1
+            return
+        next(self.repository.update(args[0], **kwargs))
+        yield f"Session updated: {args[0]}", 0
 
     def delete(self, args: list, flags: list, **kwargs):
-        session = self.repository.get(args[0])
+        session = next(self.repository.get(args[0]))
         if not session:
-            return f"Session not found: {args[0]}", 1
-        self.repository.delete(session)
-        return f"Session deleted: {args[0]}", 0
+            yield f"Session not found: {args[0]}", 1
+            return
+        next(self.repository.delete(session))
+        yield f"Session deleted: {args[0]}", 0
