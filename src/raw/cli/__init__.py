38acc13d523be334075d03e729e1fafea310367e
Dvodnenko <thekovalskii@gmail.com>
2025-11-04 14:51:25 +0200

    feat: add keyboard interrupt error handling

--- src/raw/cli/__init__.py
+++ src/raw/cli/__init__.py
@@ -23,19 +23,23 @@ def execute(callback, args, kwargs, flags):
 @parse_cli_args
 def raw(args, kwargs, flags):
 
-    if not args:
+    try:
+        if not args:
+            exit(0)
+
+        if not sys.platform.lower() in SUPPORTED_SYSTEMS:
+            print(f"Unsupported operating system: {sys.platform}")
+            exit(1)
+
+        callback = resolve_callback(args)
+
+        error_ocurred: int = 0
+
+        for response in execute(callback, args, kwargs, flags):
+            print(response[0])
+            if response[1] != 0: error_ocurred = 1
+
+        exit(error_ocurred)
+    except KeyboardInterrupt:
+        print("Cancelled by user")
         exit(0)
-
-    if not sys.platform.lower() in SUPPORTED_SYSTEMS:
-        print(f"Unsupported operating system: {sys.platform}")
-        exit(1)
-
-    callback = resolve_callback(args)
-
-    error_ocurred: int = 0
-
-    for response in execute(callback, args, kwargs, flags):
-        print(response[0])
-        if response[1] != 0: error_ocurred = 1
-
-    exit(error_ocurred)
