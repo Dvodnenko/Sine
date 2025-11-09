dcaba101deb63b04447aca6ace7e7f2b3fb78324
Dvodnenko <thekovalskii@gmail.com>
2025-11-09 18:31:22 +0200

    refactor: adjust tag service

--- src/sine/daemon/services/tags.py
+++ src/sine/daemon/services/tags.py
@@ -2,13 +2,30 @@ from ..repositories.tag import saTagRepository
 from ..repositories.folder import saFolderRepository
 from ..entities import Tag
 from ..database.funcs import get_all_by_titles
-from .decorators import provide_conf, cast_kwargs
+from .decorators import cast_kwargs
+from .base import Service
+from ...common import load_config, parse_afk
 
 
-class TagService:
+PARSER = parse_afk
+class TagService(Service):
     def __init__(self, repository: saTagRepository):
         self.repository = repository
         self.folders_repository = saFolderRepository(repository.session)
+
+    def execute(self, argv):
+        if not hasattr(self, argv[0]):
+            yield f"Command not found: {argv}", 1
+            return
+        try:
+            if len(argv) > 1:
+                args, flags, kwargs = PARSER(argv[1:])
+            else:
+                args, flags, kwargs = PARSER([])
+            gen = getattr(self, argv[0])(args=args, flags=flags, **kwargs)
+            yield from gen
+        except Exception as e:
+            yield e.args, 1
 
     @cast_kwargs(Tag)
     def create(self, args: list, flags: list, **kwargs):
@@ -19,20 +36,18 @@ class TagService:
         next(self.repository.create(tag))
         yield f"Tag created: {tag.title}", 0
     
-    @provide_conf
     def all(self, args: list, flags: list, **kwargs):
         sortby = kwargs.get("sortby", "title")
         if "t" in flags:
             for tag in self.repository.get_all(sortby):
                 yield tag.title, 0
         else:
-            pattern: str = kwargs["__cnf"]["formats"]["tag"]
+            pattern: str = load_config()["formats"]["tag"]
             for tag in self.repository.get_all(sortby):
                 yield pattern.format(**tag.to_dict()), 0
     
-    @provide_conf
     def print(self, args: list, flags: list, **kwargs):
-        pattern: str = kwargs["__cnf"]["formats"]["tag"]
+        pattern: str = load_config()["formats"]["tag"]
         for tag in get_all_by_titles(self.repository.session, Tag, args):
             yield pattern.format(**tag.to_dict()), 0
     
