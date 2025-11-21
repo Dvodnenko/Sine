446843541aa6297b3e938821215db44bccecc724
Dvodnenko <thekovalskii@gmail.com>
2025-11-21 17:16:35 +0200

    feat: add note entity

--- src/sine/daemon/entities/__init__.py
+++ src/sine/daemon/entities/__init__.py
@@ -3,10 +3,11 @@ from .folder import Folder
 from .session import Session
 from .tag import Tag
 from .task import Task
+from .note import Note
 from .enums import Color, TaskStatus
 
 
 __all__ = [
     "Entity", "Folder", "Session", "Tag", 
-    "Color", "Task", "TaskStatus",
+    "Color", "Task", "TaskStatus", "Note",
 ]
